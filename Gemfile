534756e2dc54345df3c44223c8bf55549f24d739
test <test@ubuntu.(none)>
2013-10-12 12:09:07 -0700

    Initiale improvement

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-activerecord'
+gem 'sinatra-assetpack'
+
+group :development do
+	gem 'thin'
+end
+
+group :test do
+	gem 'rspec'
+	gem 'capybara'
+end
---
+++ sintde.rb
@@ -0,0 +1,38 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'sinatra/assetpack'
+
+class UberUrlShortener < Sinatra::Base
+	set :root, File.dirname(__FILE__)
+	set :bind, '0.0.0.0'
+
+	register Sinatra::AssetPack
+
+	enable :inline_templates
+
+	assets do
+		serve '/js',	from: 'app/js'
+		serve '/css',	from: 'app/css'
+		serve '/image',	from: 'app/image'
+
+		css :cssapp, [
+			'/css/*.css'
+		]
+
+		js :jsapp, [
+			'/js/*.js'
+		]
+
+		css_compression :simple
+		js_compression :jsmin
+	end
+
+	get '/' do
+		erb :index
+	end
+
+run! if app_file == $0
+
+end
+
+
